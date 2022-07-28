430c4cb6a8ea6854fc6c1e9414088b8e99cbc385
nguyenthanhchung090302 <110025385+nguyenthanhchung090302@users.noreply.github.com>
2022-07-28 21:42:53 +0700

    Add files via upload

---
+++ BTVN/btvn1.js
@@ -0,0 +1,24 @@
+// bài 1
+// let a = Number(prompt("nhập điểm môn vật lý"));
+// let b = Number(prompt("nhập điển môn hóa học"));
+// let c = Number(prompt("nhập điển môn sinh hoc"));
+// let d = a + b + c;
+// let e = (a + b + c)/3;
+// alert(d);
+// alert(e);
+
+// bài 2
+// let m =(prompt("nhập độ C cần đổi"));
+// let n = ( 1.8 * m + 32 );
+// alert(n);
+
+//bài 3
+// let a = prompt("nhập bán kính của hình tròn");
+// let c =(a * a * Math.PI);
+// alert(c);
+
+// bài 4
+// let h = prompt("nhập chu vi hình tròn");
+// let j =(2);
+// let Q =(j * h * Math.PI);
+// alert("chu vi hình tròn là" + Q);
